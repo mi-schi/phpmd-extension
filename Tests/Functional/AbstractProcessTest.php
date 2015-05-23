a87c20c390d5541cda989a8217cb2f900557f066
Michael Schindler <madsn@gmx.de>
2015-05-23 11:00:10 +0200

    Add functional test for symfony2 controller method name rule

---
+++ Tests/Functional/AbstractProcessTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MS\PHPMD\Tests\Functional;
+
+use Symfony\Component\Process\ProcessBuilder;
+use Symfony\Component\Process\Process;
+
+/**
+ * Class AbstractProcessTest
+ *
+ * @package MS\PHPMD\Tests\Functional
+ */
+class AbstractProcessTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $filename
+     * @param $ruleset
+     *
+     * @return Process
+     */
+    protected function runPhpmd($filename, $ruleset)
+    {
+        $processBuilder = new ProcessBuilder();
+        $processBuilder->setPrefix(__DIR__ . '/../../vendor/bin/phpmd');
+
+        $processBuilder
+            ->add(__DIR__ . '/../Fixtures/' . $filename)
+            ->add('text')
+            ->add(__DIR__ . '/../../Rulesets/' . $ruleset);
+
+        $process = $processBuilder->getProcess();
+        $process->run();
+
+        return $process;
+    }
+}
---
+++ Tests/Functional/Symfony2/ControllerMethodNameTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MS\PHPMD\Tests\Functional\Symfony2;
+
+use MS\PHPMD\Tests\Functional\AbstractProcessTest;
+
+/**
+ * Class ControllerMethodNameTest
+ *
+ * @package PHPMD\Tests\Symfony2
+ */
+class ControllerMethodNameTest extends AbstractProcessTest
+{
+    /**
+     * @covers PHPMD\Rule\Symfony2\ControllerMethodName
+     */
+    public function testRule()
+    {
+        $output = $this
+            ->runPhpmd('FooController.php', 'symfony2.xml')
+            ->getOutput();
+
+        $this->assertContains('FooController.php:10	The method name have to end with Action in this controller.', $output);
+    }
+
+    /**
+     * @covers PHPMD\Rule\Symfony2\ControllerMethodName
+     */
+    public function testRuleWIthAbstract()
+    {
+        $output = $this
+            ->runPhpmd('AbstractFooController.php', 'symfony2.xml')
+            ->getOutput();
+
+        $this->assertEmpty(trim($output));
+    }
+}
