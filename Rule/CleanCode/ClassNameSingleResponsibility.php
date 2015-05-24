a781d109e9d79035e96c95f6bc7293fb49bc96d0
Michael Schindler <madsn@gmx.de>
2015-05-24 13:37:18 +0200

    Add new rule for class name

---
+++ Rule/CleanCode/ClassNameSingleResponsibility.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MS\PHPMD\Rule\CleanCode;
+
+use PDepend\Source\AST\ASTClass;
+use PHPMD\AbstractNode;
+use PHPMD\AbstractRule;
+use PHPMD\Node\ClassNode;
+use PHPMD\Node\MethodNode;
+use PHPMD\Rule\ClassAware;
+
+/**
+ * Class ClassNameSingleResponsibility
+ *
+ * Try to avoid general suffixes like Manager. It might violate the single responsibility principle.
+ *
+ * @package MS\PHPMD\Rule\CleanCode
+ */
+class ClassNameSingleResponsibility extends AbstractRule implements ClassAware
+{
+    /**
+     * @param AbstractNode $node
+     */
+    public function apply(AbstractNode $node)
+    {
+        if (!$node instanceof ClassNode) {
+            return;
+        }
+
+        $suffixes = $this->getStringProperty('suffixes');
+        $generalSuffixes = explode($this->getStringProperty('delimiter'), $suffixes);
+
+        foreach ($generalSuffixes as $generalSuffix) {
+            if ($generalSuffix === substr($node->getImage(), strlen($generalSuffix) * -1)) {
+                $this->addViolation($node, [
+                    $suffixes,
+                    $generalSuffix
+                ]);
+                break;
+            }
+        }
+    }
+}
