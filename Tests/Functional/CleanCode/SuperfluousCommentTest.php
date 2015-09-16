d2774bccdadf1fa76126c86d91ae6190d25a19d0
Michael Schindler <madsn@gmx.de>
2015-09-16 15:40:40 +0200

    Make test method name understandable

--- Tests/Functional/CleanCode/SuperfluousCommentTest.php
+++ Tests/Functional/CleanCode/SuperfluousCommentTest.php
@@ -14,13 +14,13 @@ class SuperfluousCommentTest extends AbstractProcessTest
     /**
      * @covers MS\PHPMD\Rule\CleanCode\SuperfluousComment
      */
-    public function testRule()
+    public function testSuperfluousCommentRule()
     {
         $output = $this
             ->runPhpmd('Service/SuperfluousComment.php', 'cleancode.xml')
             ->getOutput();
 
-        $this->assertNotContains('SuperfluousComment.php:42', $output);
+        $this->assertNotContains('SuperfluousComment.php:42	It seems', $output);
 
         $this->assertContains('SuperfluousComment.php:10	It seems that the class has a superfluous comment description. It fits 88 percent with the name of that.', $output);
         $this->assertContains('SuperfluousComment.php:10	It seems that the property $name has a superfluous comment description. It fits 73 percent with the name of that.', $output);
