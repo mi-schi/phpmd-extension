b4a164be3ce7b44ed83620a94148c099d89fcebb
Michael Schindler <madsn@gmx.de>
2015-09-22 15:07:47 +0200

    Add new rule for public methods in traits

---
+++ Rule/CleanCode/TraitPublicMethod.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MS\PHPMD\Rule\CleanCode;
+
+use PDepend\Source\AST\ASTMethod;
+use PHPMD\AbstractNode;
+use PHPMD\AbstractRule;
+use PHPMD\Node\MethodNode;
+use PHPMD\Node\TraitNode;
+use PHPMD\Rule\MethodAware;
+
+/**
+ * Class TraitPublicMethod
+ *
+ * The purpose of a trait should be the reuse of methods which help the basic classes. Make your code clearly and define interfaces of your class as public methods.
+ *
+ * @package MS\PHPMD\Rule\CleanCode
+ */
+class TraitPublicMethod extends AbstractRule implements MethodAware
+{
+    /**
+     * @param AbstractNode|MethodNode|ASTMethod $node
+     */
+    public function apply(AbstractNode $node)
+    {
+        if ($node->getParentType() instanceof TraitNode && true === $node->isPublic()) {
+            $this->addViolation($node);
+        }
+    }
+}
